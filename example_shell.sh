ff5d04eb4a564f8ff3678e7f53b5c00837cbd6fa
Jake Peterson <60857922+snakepeterson@users.noreply.github.com>
2022-11-15 09:57:00 -0600

    Add files via upload

---
+++ example_shell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --partition=biocrunch
+#SBATCH --nodelist=whatever
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=cobretti
+#SBATCH --mail-user=[example]@iastate.edu
+#SBATCH --mail-type=ALL
+
+module load py-biopython/1.70-py3-wos466g
+module load python/3.6.5-fwk5uaj
+
+python cobretti.py -stage 1A -email [example]@iastate.edu
